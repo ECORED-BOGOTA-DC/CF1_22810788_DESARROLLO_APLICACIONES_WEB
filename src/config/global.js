c32fec1c31336c5a9251dd10975cb6208570e7af
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2023-10-10 08:36:12 -0500

    Correcciones 7

--- src/config/global.js
+++ src/config/global.js
@@ -178,7 +178,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/material.pdf',
+        download: 'downloads/22810788_CF001_DU.zip',
       },
       {
         icono: 'fas fa-download',
